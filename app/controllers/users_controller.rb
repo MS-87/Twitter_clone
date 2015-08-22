2f0d99a16088f7ee0bee244ec4f97c15383d9445
MS_87 <salvoch.manuel@gmail.com>
2015-08-22 01:57:42 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    #we need this because the <form> tag in our view needs access to a User
+    #object 
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      #this is the same as "redirect_to user_url(@user)"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                      :password_confirmation)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   
   get 'users/new'
-
+  
+  #Routes for our pages:
   root              'static_pages#home'
   get 'help'    =>  'static_pages#help'
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
   get 'signup'  =>  'users#new'
+  
+  
+  #"resources" provides all REST links for our resource... User
+  resources :users
 
+
+  #----
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
